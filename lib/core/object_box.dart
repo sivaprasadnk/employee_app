aaa4ec25fd8e1c69ff1ba7c59939beb3701e1c38
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-02-06 17:17:52 +0530

    validation details correction

--- lib/core/object_box.dart
+++ lib/core/object_box.dart
@@ -1,16 +1,11 @@
-import 'package:employee_app/data/models/employee_model.dart';
 import 'package:employee_app/objectbox.g.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
 class ObjectBox {
-  /// The Store of this app.
   late final Store store;
-  late final Box<EmployeeModel> _employeeBox;
 
-  ObjectBox._create(this.store) {
-    _employeeBox = Box<EmployeeModel>(store);
-  }
+  ObjectBox._create(this.store);
 
   static Future<ObjectBox> create() async {
     final docsDir = await getApplicationDocumentsDirectory();
